db5b12c7d67d34aa0ce75587d47a440581a9ef80
JoMei9019 <110409669+JoMei9019-real@users.noreply.github.com>
2023-05-28 14:45:16 +0200

    Update python.py

--- python.py
+++ python.py
@@ -6,7 +6,7 @@ window.title("Titel")
 lbl = Label(window, text="By JoMei9019", fg='green', font=("Helvetica", 12))
 lbl.place(x=66, y=80)
 window.geometry("400x250")
-#window.resizable(False,False)
+window.resizable(False,False)       #Delete line, if window should be resizable!
 
 
 window.mainloop()
