c336e5aa5909317cef72b0c84b2cbc8d0b24bba9
JoMei9019 <110409669+JoMei9019-real@users.noreply.github.com>
2023-05-28 14:44:35 +0200

    Update python.py

--- python.py
+++ python.py
@@ -7,7 +7,6 @@ lbl = Label(window, text="By JoMei9019", fg='green', font=("Helvetica", 12))
 lbl.place(x=66, y=80)
 window.geometry("400x250")
 #window.resizable(False,False)
-#window.iconphoto(False, tk.PhotoImage(file=resource_path("Logo.png")))
 
 
 window.mainloop()
